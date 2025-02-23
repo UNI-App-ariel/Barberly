1c7d5dfcb7bcd0d1a03f1d45473752570d15acab
Zoabi Abedallah <122079462+abodezoabi@users.noreply.github.com>
2025-02-23 14:53:32 +0200

    Updated lib

--- lib/features/customer/domain/usecases/get_favorite_shops.dart
+++ lib/features/customer/domain/usecases/get_favorite_shops.dart
@@ -4,7 +4,7 @@ import 'package:uni_app/core/common/domian/entities/barbershop.dart';
 import 'package:uni_app/core/errors/failures.dart';
 import 'package:uni_app/core/usecase/usecase.dart';
 import 'package:uni_app/features/customer/domain/repsitories/favorites_repo.dart';
-
+// class returns fav shops
 class GetFavoriteShopsUseCase implements UseCase<List<Barbershop>, String> {
   final FavoritesRepo favoritesRepo;
 
